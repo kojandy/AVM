9cd3a759557e386f08efb94fc320f3f2327a2fca
sjoon2455 <sjoon2455@kaist.ac.kr>
2019-05-29 16:55:52 +0900

    almost done

--- minLength.py
+++ minLength.py
@@ -1,6 +1,13 @@
 # input: .py - target function
 # output: integer - minimum length of string input
+'''
+1. if 문 안에 contains(n개의 캐릭터), else는 할 필요가 없음!!!
+    --> n을 구한다. --> 변수와 비교하여 n이 더 크면 업데이트
+2. if or body에서 concat 나오면 리턴 ㄱ ㄱ
+'''
+
 import astor
+import ast
 from tree_walk import * 
 
 def fun(a, b, c):
@@ -16,27 +23,25 @@ class ChangeIf(TreeWalk):
         body = self.cur_node
         for i, child in enumerate(body[:]):
             self.walk(child)
+            if child contains concat:
+                break
             if isinstance(child, ast.If):
-                count.incr()
                 lhs = body[i].test.left.id
                 op = body[i].test.ops[0]
                 rhs = body[i].test.comparators[0]
-                depth = count.getCount()
-                traceFunction = trace.whichTracetoUse(op, lhs, rhs)
-                args1 = ast.Num()
-                args1.n = depth
-                args2 = ast.Str(lhs)
-                args3 = rhs
-                body[i].test = ast.Call(ast.Name("traceFunction"), [args1, args2, args3], [])
+                n = len(rhs)
+                compUpdate(temp, n)
         return True
 
     def pre_Call(self):
         self.__name = self.cur_node.func.id
         return True
 
-walker = ChangeIf()
-walker.walk(_ast)
-
-
 def minLength(f):
     _ast = astor.code_to_ast(f)
+    walker = ChangeIf()
+    walker.walk(_ast)
+    __ast = astor.to_source(_ast)
+    return __ast
+
+minLength(fun)
