e12968ae9384264d1b6f9cb25bb50c04fafd3546
Ohjun Kwon <dev@kojandy.com>
2019-06-05 17:49:44 +0900

    Add some cases

--- target.py
+++ target.py
@@ -1,6 +1,13 @@
 def fun(a, b):
+    if a == a:
+        pass
+    if 'abc' == 'abc':
+        pass
+    if a < 'aaaa':
+        if a < 'zzzz':
+            pass
     if a == 'aaaa':
-        if b == 'bbbb':
+        if b < 'bbbb':
             pass
         if b != 'asdf':
             pass
